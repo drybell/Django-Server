c68fe7a3d366667b37f5113953f5a0365ed3e7d9
Daniel Ryaboshapka <dan4821@gmail.com>
2020-10-07 13:57:05 -0400

    fix settings

--- preview/wsgi.py
+++ preview/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'preview.preview.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'preview.settings')
 
 application = get_wsgi_application()
